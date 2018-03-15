23568d643cee23d37d066b6d57319612a0db94df
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-03-15 18:28:28 +0530

    added user profile logic

--- server/utils/s3buckethandler.js
+++ server/utils/s3buckethandler.js
@@ -1,30 +1,18 @@
 import AWS from 'aws-sdk';
 import logger from 'winston';
 import fs from 'fs';
-//import proxy from 'proxy-agent';
+
 
 const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || "abc123";
 const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || "abc123";
 const BUCKET_NAME = 'qsapien';
 
-// httpOptions: { agent: proxy('http://satyam_bansal:Sat%4001022018@ptbc1.persistent.co.in:8080') },
-// httpsOptions: { agent: proxy('http://satyam_bansal:Sat%4001022018@ptbc1.persistent.co.in:8080') }
-// console.log('access key::'+SECRET_ACCESS_KEY);
-// console.log('key id::'+ACCESS_KEY_ID);
-// accessKeyId: ACCESS_KEY_ID,
-// secretAccessKey: SECRET_ACCESS_KEY,
 
 AWS.config.update({
 sslEnabled:false,
 region:'ap-south-1'
 });
 let s3=new AWS.S3();
-// let s3Bucket = new AWS.S3({
-//   accessKeyId: ACCESS_KEY_ID,
-//   secretAccessKey: SECRET_ACCESS_KEY,
-//   Bucket: BUCKET_NAME,
-//   region:'us-east-1'
-// });
 exports.uploadFileToS3 = (fileBody, fileName, callback) => {
   //let fileData=fs.readFileSync(filePath);
   fs.writeFileSync("111",fileBody);
