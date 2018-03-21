001ebe5710d8078c28a481668264d9611fc1cef1
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-03-21 21:04:00 +0530

    commit

--- src/components/Portal/socketapi.js
+++ src/components/Portal/socketapi.js
@@ -1,6 +1,7 @@
 import socket from 'socket.io-client';
 // import logger from 'winston';
-let client=socket('http://localhost:3001');
+const REACT_APP_API_URL=process.env.REACT_APP_API_URL||'httpL//localhost:3001'
+let client=socket(REACT_APP_API_URL);
 
 export function sendChallengeSocketApi({values}){
 // logger.info('inside send challenge socket api');
