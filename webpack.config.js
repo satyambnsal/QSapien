d31e364076400b6fea9027bbb8d29d5ae718739a
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-03-21 19:49:42 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
 const webpack=require('webpack');
 const path=require('path');
+const BUILD_DIR=path.resolve(__dirname,'./public/build');
+const APP_DIR=path.resolve(__dirname,'./src');
 module.exports={
-    context:__dirname,
-    entry:"./src/main.js",
+    entry:APP_DIR+"/index.js",
     output:{
-        filename:"./build/bundle.js"
+        filename:"bundle.js",
+        path:BUILD_DIR
     },
-    devServer:{
-        inline:true,
-        contentBase:'./src',
-        port:8080
-    },
-    watch:true,
+
     module:{
-        loaders:[
-            {
-                test:/\.js$/,
-                exclude:/node_modules/,
-                loader:'babel-loader'
-            }
-        ]
+    rules:[
+        {
+            test:/\.css$/,
+            use:[{
+                loader:'style-loader'
+            },{
+                loader:'css-loader'
+            }]
+        },
+        {
+            test:/\.js$/,
+            exclude:/node_modules/,
+            use:[
+                {
+                    loader:'babel-loader',
+                    options:{
+                        cacheDirectory:true,
+                        presets:['react','es2015','stage-2']
+                    }
+                }
+            ]
+        }
+    ]
     }
 }
