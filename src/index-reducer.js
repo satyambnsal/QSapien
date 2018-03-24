1351b92ba633fc5eefebc70d8ffc71cba4662e72
Satyam Bansal <satyamsgsits1994@gmail.com>
2018-03-24 10:15:14 +0530

    last name undefined value fix

--- src/index-reducer.js
+++ src/index-reducer.js
@@ -1,5 +1,4 @@
 import {combineReducers} from 'redux';
-import {reducer as form} from 'redux-form';
 import client from './components/Client/reducer';
 import login from './components/Login/reducer'; 
 import signup from './components/Signup/reducer';
@@ -8,7 +7,6 @@ var IndexReducer=combineReducers({
     client,
     login,
     signup,
-    form,
     portal
 });
 export default IndexReducer;
--- webpack.config.js
+++ webpack.config.js
@@ -9,32 +9,18 @@ module.exports = {
     entry: APP_DIR + "/index.js",
     output: {
         filename: "bundle.[hash].js",
-        path:APP_DIR+'/public'
+        path:BUILD_DIR
     },
 
     module: {
         rules: [
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true
-                    }
-                }]
-            },
             {
                 test: /.css$/,
                 use: [{
                     loader: 'style-loader'
                 },
                 {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                        camelCase: true,
-                        sourceMap: true
-                    }
+                    loader: 'css-loader'
                 }
                 ]
             },
@@ -52,7 +38,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './public/index.html'),
-            filename: path.resolve(__dirname, './public/index.html')
+            filename: 'index.html'
         })
     ],
     devServer: {
